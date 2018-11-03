83e55b379d8e15a5849ddb665a0a174eaede3ce9
zetkno <jiuminghan@163.com>
2018-11-03 12:01:26 +0800

    cj

--- src/main/java/com/zetkno/findbook/controller/TestContorller.java
+++ src/main/java/com/zetkno/findbook/controller/TestContorller.java
@@ -1,16 +1,16 @@
 package com.zetkno.findbook.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zetkno on 2018/11/3 0003.
  */
-@RequestMapping("/test")
+@RestController
+//@RequestMapping(value= "/test")
 public class TestContorller {
 
-    public static void main(String[] args) {
-
-    }
+    @RequestMapping(value= "/test")
     public String test() {
         return "test success";
     }

652cc1ff4d10371a21049343282086e8a8c0127c
zetkno <jiuminghan@163.com>
2018-11-05 22:52:57 +0800

    登录未完成

--- src/main/java/com/zetkno/findbook/FindbookApplication.java
+++ src/main/java/com/zetkno/findbook/FindbookApplication.java
@@ -3,8 +3,10 @@ package com.zetkno.findbook;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:kaptcha.xml"})
 public class FindbookApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zetkno/findbook/controller/LoginController.java
+++ src/main/java/com/zetkno/findbook/controller/LoginController.java
@@ -1,16 +1,49 @@
 package com.zetkno.findbook.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
 
 @RestController
+@RequestMapping("/")
 public class LoginController {
+
+    @Autowired
+    private Producer kaptchaProducer = null;
+
+    @RequestMapping("/kaptcha")
+    public void getKaptchaImage(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        HttpSession session = request.getSession();
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("image/jpeg");
+//        生产验证码
+        String kapText = kaptchaProducer.createText();
+        session.setAttribute(Constants.KAPTCHA_SESSION_KEY, kapText);
+//        向客户端写出
+        BufferedImage bi = kaptchaProducer.createImage(kapText);
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(bi, "jpg", out);
+        try {
+            out.flush();
+        } finally {
+            out.close();
+        }
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
